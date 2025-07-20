f3c7a0086f4b66ff71cae0dfcf11478399b1878d
Sara <sarailic431@gmail.com>
2025-07-20 18:43:58 +0200

    .      .

--- src/main.jsx
+++ src/main.jsx
@@ -9,16 +9,20 @@ import {
 } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-// const router = createBrowserRouter([]);
-
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <RouterProvider router={router}> */}
     <QueryClientProvider client={queryClient}>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
     </QueryClientProvider>
-    {/* </RouterProvider> */}
   </StrictMode>
 );
