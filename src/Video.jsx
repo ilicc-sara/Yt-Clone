f91b1d23c781d8f2d43372c63dc7cd65754790f1
Sara <sarailic431@gmail.com>
2025-07-23 10:27:45 +0200

    fetching video data

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,9 +1,40 @@
 import { useParams } from "react-router-dom";
 import React from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
 function Video(props) {
   const params = useParams();
-  return <h1>Video {params.videoId} </h1>;
+  const { data, isPending, isLoading, error, isFetching } = useQuery({
+    queryKey: ["videos", params.videoId],
+    queryFn: () => renderVideo(params.videoId),
+  });
+
+  console.log(data);
+  return (
+    <>
+      {isPending && <h1>Loading...</h1>}
+      <h1>Video {params.videoId} </h1>
+    </>
+  );
 }
+
+const renderVideo = async (id) => {
+  const response = await fetch(
+    // `https://youtube-v31.p.rapidapi.com/search?q=${id}&part=snippet,id&maxResults=24&regionCode=US`,
+    `https://youtube-v31.p.rapidapi.com/videos?part=contentDetails,snippet,statistics&id=${id}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "be6fa48021msh06130b56f1fc57bp1a07f5jsn0641dee67229",
+        "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+      },
+    }
+  );
+  return await response.json();
+};
 
 export default Video;
