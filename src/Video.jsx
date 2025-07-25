c09e25decdadf6c058f54f7d1d02b863f2c7768b
Sara <sarailic431@gmail.com>
2025-07-25 11:05:42 +0200

    ... . ... . ... . ...

--- src/Video.jsx
+++ src/Video.jsx
@@ -9,38 +9,49 @@ import YouTube from "react-youtube";
 
 function Video(props) {
   const params = useParams();
-  const { data, isPending, isLoading, error, isFetching } = useQuery({
+  // const { data, isPending, isLoading, error, isFetching } = useQuery({
+  //   queryKey: ["videos", params.videoId],
+  //   queryFn: () => renderVideo(params.videoId),
+  // });
+
+  const videoQuery = useQuery({
     queryKey: ["videos", params.videoId],
     queryFn: () => renderVideo(params.videoId),
   });
 
-  console.log(data);
+  // const commentsQuery = useQuery({
+  //   queryKey: ["comments", params.videoId],
+  //   queryFn: () => renderComments(params.videoId),
+  // });
+
+  // // console.log(videoQuery.data);
+  // console.log(commentsQuery.data);
   return (
     <>
-      {isPending && <h1>Loading...</h1>}
+      {videoQuery.isPending && <h1>Loading...</h1>}
       {/* <h1>Video {params.videoId} </h1> */}
 
-      {data && (
+      {videoQuery.data && (
         <div className="video-display">
           <YouTube
             videoId={params.videoId}
             opts={{ height: "500px", width: "900px" }}
           />
 
-          <h1> {data.items[0].snippet.title} </h1>
+          <h1> {videoQuery.data.items[0].snippet.title} </h1>
 
           <div className="video-display-info">
             <p className="views">
               {" "}
-              {Number(data.items[0].statistics.viewCount).toLocaleString(
-                "en-US"
-              )}{" "}
+              {Number(
+                videoQuery.data.items[0].statistics.viewCount
+              ).toLocaleString("en-US")}{" "}
               views
             </p>
 
             <p>
               <ion-icon name="thumbs-up-outline"></ion-icon>{" "}
-              {data.items[0].statistics.likeCount}{" "}
+              {videoQuery.data.items[0].statistics.likeCount}{" "}
             </p>
             <ion-icon name="thumbs-down-outline"></ion-icon>
             <p>
@@ -72,5 +83,19 @@ const renderVideo = async (id) => {
   const response = await fetch(url, options);
   return await response.json();
 };
+
+// const renderComments = async (id) => {
+//   const commentsUrl = `https://youtube-v31.p.rapidapi.com/commentThreads?part=snippet&videoId=${id}&maxResults=100`;
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "x-rapidapi-key": "d9efc1fe32msh16930bf036ff25cp19e4d2jsn0888502f5ab3",
+//       "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+//     },
+//   };
+
+//   const response = await fetch(commentsUrl, options);
+//   return await response.json();
+// };
 
 export default Video;
