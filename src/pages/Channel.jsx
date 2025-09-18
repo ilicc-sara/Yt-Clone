7efe4e6e267f2784385f612dd1040047e236175c
Sara <sarailic431@gmail.com>
2025-09-18 18:38:03 +0200

    . . . . . .....

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,17 +1,31 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Videos from "../UI/Videos";
 import { channelResponse, channelVideosResponse } from "@/api/api";
 import useChannelData from "@/api/useChannelData";
+import { ToastContainer, toast } from "react-toastify";
 
 function Channel() {
   const params = useParams();
 
   const { channelQuery, channelVideosQuery } = useChannelData(params.channelId);
 
+  useEffect(() => {
+    if (channelQuery.error) {
+      toast.error("Failed to load channel data...");
+    }
+  }, [channelQuery.error]);
+
+  useEffect(() => {
+    if (channelVideosQuery.error) {
+      toast.error("Failed to load channel videos...");
+    }
+  }, [channelVideosQuery.error]);
+
   return (
     <div className="clicked-channel-container">
       {channelQuery.isPending && <div className="loader"></div>}
-      {channelQuery.error && <h1>Error...Something went wrong</h1>}
+      {channelQuery.error && <h1>Error...Something went wrong</h1>}{" "}
       <img
         className="channel-img"
         src={channelQuery.data?.items[0].snippet.thumbnails.medium.url}
@@ -22,16 +36,16 @@ function Channel() {
       <p className="channel-description">
         {channelQuery.data?.items[0].snippet.description}
       </p>
-
       <div className="channel-videos-container">
         <h1 className="displayed-channel-videos">
           {channelQuery.data?.items[0].snippet.title}'s{" "}
           <span className="channel-videos-span">videos</span>
         </h1>
         {channelVideosQuery.isPending && <div className="loader"></div>}
-        {channelVideosQuery.error && <h1>Error...Something went wrong</h1>}
+        {channelVideosQuery.error && <h1>Error...Something went wrong</h1>}{" "}
         <Videos data={channelVideosQuery.data} />
       </div>
+      <ToastContainer position="top-center" />
     </div>
   );
 }
